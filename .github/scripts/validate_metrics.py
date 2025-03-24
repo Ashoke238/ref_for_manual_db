23a593fa338d39e67b1e7ee1b89520dea3707b71
Ashoke238 <Ashoke238@users.noreply.github.com>
2025-03-24 00:29:15 -0400

    Update validate_metrics.py

--- .github/scripts/validate_metrics.py
+++ .github/scripts/validate_metrics.py
@@ -10,7 +10,7 @@ user_email = os.getenv("USER_EMAIL")
 env = "prod" if branch == "main" else "dev"
 
 # Build dynamic experiment name
-experiment_name = f"/Users/{user_email}/{repo}_train_{env}"
+experiment_name = f"/Users/{user_email}/{repo}-train-{env}"
 print(f"🔍 Checking experiment: {experiment_name}")
 
 # Query MLflow
